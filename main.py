7b53247fead24955bcd7ca1d47e07b7e888250c0
student <student@PC-H-04>
2023-05-25 20:13:53 +0300

    3 завдання

---
+++ main.py
@@ -0,0 +1,12 @@
+#3
+import  requests
+from bs4 import BeautifulSoup
+response = requests.get('https://uk.wikipedia.org/')
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features= "html.parser")
+    soup_list = soup.find_all('a')
+    for link in soup_list:
+        href = link.get("href")
+        print(href)
+        if href.startswith('https://'):
+            print(href)
