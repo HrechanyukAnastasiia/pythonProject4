68c97bb3b1dae1e7ce64d94ab58f4e45ff2b63c6
student <student@PC-H-04>
2023-05-25 20:16:00 +0300

    3

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #3
 import  requests
 from bs4 import BeautifulSoup
-response = requests.get('https://uk.wikipedia.org/')
+response = requests.get('https://www.example.com/')
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, features= "html.parser")
     soup_list = soup.find_all('a')
     for link in soup_list:
-        href = link.get("href")
+        href = link.get('href')
         print(href)
         if href.startswith('https://'):
             print(href)
