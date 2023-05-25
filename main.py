368a7747001bf944ce2fbfb20e724a6a6065a9de
student <student@PC-H-04>
2023-05-25 20:45:22 +0300

    Бонус

---
+++ main.py
@@ -0,0 +1,11 @@
+import  requests
+from bs4 import BeautifulSoup
+response = requests.get('https://weather.com/uk-UA/weather/'
+                        'today/l/2261b41a35498e667410cb84dfb'
+                        '95d455f977b64d2c02b673df410b7d9ec31ed')
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features= "html.parser")
+    city = soup.find('h1',{"class":"CurrentConditions--location--1YWj_"})
+    if city:
+        city_name = city.text.strip()
+        print("Місто не знайдено")
