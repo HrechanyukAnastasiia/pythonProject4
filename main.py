3026426fed816036a6b8a66eb45858aa88837d6b
student <student@PC-H-04>
2023-05-25 20:29:59 +0300

    4 завдання

---
+++ main.py
@@ -0,0 +1,12 @@
+#4
+import  requests
+from bs4 import BeautifulSoup
+response = requests.get('https://www.example.com/')
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features= "html.parser")
+    for script in soup.find_all(["style", "script"]):
+        script.extract()
+    text = ' '.join(soup.stripped_strings)
+    words = len(text.split())
+    print(words)
