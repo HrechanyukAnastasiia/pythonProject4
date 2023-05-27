8ed1622cfb47bcea815c7b9e4506c8de6a901445
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-27 10:11:32 +0300

    3 завдання

---
+++ main.py
@@ -0,0 +1,17 @@
+#3
+class InvalidFileFormatError(Exception):
+    def __init__(self, f):
+        self.f = f
+def read_file(f):
+    try:
+        with open(f, "r") as file:
+            content = file.read()
+            print("Вміст файлу", content)
+    except IOError:
+        raise InvalidFileFormatError(f)
+
+try:
+    read_file(input("Введіть назву файлу:"))
+except InvalidFileFormatError as a:
+    print(f"Невірний формат файлу {a.f} \n"
+          f"Підримуються тільки текстові файли")
