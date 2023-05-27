701440364296ca0a7f59a14eaa16449bdae5c5a9
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-27 10:16:13 +0300

    1 завдання

---
+++ main.py
@@ -0,0 +1,17 @@
+#1
+class InvalidUrlError(Exception):
+    def __init__(self, url):
+        self.url = url
+def fetch_data_from_url(url):
+    if url != "https://mystat.itstep.org/ua/main/homework/page/index":
+        raise InvalidUrlError(url)
+    else:
+        print("Успішно!")
+        print()
+try:
+    print("Вхід до майстату")
+    url = input("Введіть ключ до сайту Mystat:")
+    fetch_data_from_url(url)
+except InvalidUrlError as a:
+    print(f"Невірна силка {a.url}\n"
+          f"Введіть силку правильно")
